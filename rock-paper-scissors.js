4d118a05348e31e58130a1fd8c829d2f9311cb41
noobsta98 <palparthajit@yahoo.com>
2024-06-14 21:18:34 +0530

    revert back to previous build

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // const ROUNDS = 5;
+  const ROUNDS = 5;
 
 // INIT computerScore and humanScore
 let computerScore = 0;
 let humanScore = 0;
 
 // PLAY game 
-// function playGame(){
-//   for(let i = 0; i < ROUNDS; i++){
-//   playRound();
-//   }
-// }
+function playGame(){
+  for(let i = 0; i < ROUNDS; i++){
+  playRound();
+  }
+}
 
 // CHECK score, DECLARE winner
 let declareWinner = (cScore, hScore) => {
@@ -40,9 +40,24 @@ function getComputerChoice(){
 //   return humanChoice;
 // }
 
+function getHumanChoice(){
+  container.addEventListener('click', (e) =>{
+    let target = e.target;
+  
+    switch (target.id) {
+      case 'rock':
+      case 'paper':
+      case 'scissor':
+        // console.log(target.textContent)
+        playRound(target.textContent.isLowerCase());
+        break;
+    }
+  });
+}
+
 // CREATE function to playRound
-function playRound(pPick){
-  pPick = pPick.toLowerCase();
+function playRound(){
+  let pPick = getHumanChoice();
   let cPick = getComputerChoice();
   console.log(`Player: ${pPick}`);
   console.log(`Computer: ${cPick}`);
@@ -69,17 +84,7 @@ const result = document.querySelector('p');
 const text = document.createTextNode(`PLAYER SCORE: ${humanScore}
 COMPUTER SCORE: ${computerScore}`);
 result.appendChild(text);
-container.addEventListener('click', (e) =>{
-  let target = e.target;
 
-  switch (target.id) {
-    case 'rock':
-    case 'paper':
-    case 'scissor':
-      // console.log(target.textContent)
-      playRound(target.textContent);
-      break;
-  }
-});
+playGame();
 // declareWinner(computerScore, humanScore);
 });
